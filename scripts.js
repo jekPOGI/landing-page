afdb5c9a0d217743197d00bc2752201a289d2c71
jekPOGI <164014709+jekPOGI@users.noreply.github.com>
2024-08-29 16:50:36 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function signUp() {
+    alert('Sign Up functionality coming soon!');
+}
+
+function login() {
+    alert('Login functionality coming soon!');
+}
